955c1fa583ca15785a80b3a4ced68207bcd617b0
hashirafzal <hashir.afzal@sltn.net>
2025-02-05 17:09:15 +0500

    Initial commit.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        System.out.println("Hello, World!");
+        System.out.println("This is the Start of new Project , and this is the first commit.");
+    }
+}
